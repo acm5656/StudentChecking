966b5b214b2783285de696d204bfb1f27cbd5c8d
acm <807603148@qq.com>
2017-07-10 09:37:42 +0800

    完成了导员的基本UI，并且能够登录注册，查询请假申请

--- app/src/main/java/com/example/checkingsystem/net/AssistantHandlerLeaveNet.java
+++ app/src/main/java/com/example/checkingsystem/net/AssistantHandlerLeaveNet.java
@@ -1,12 +1,14 @@
 package com.example.checkingsystem.net;
 
 import android.app.Activity;
+import android.content.Context;
 
 /**
  * Created by 那年.盛夏 on 2017/7/10.
  */
 
+
 public class AssistantHandlerLeaveNet {
     Activity activity;
-
+    Context context;
 }
