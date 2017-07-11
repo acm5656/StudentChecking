5b8afd31f0844b0e6f0d383daea5845a9af8a2a8
acm <807603148@qq.com>
2017-07-11 20:25:34 +0800

    进行合并

--- app/src/main/java/com/example/checkingsystem/ChooseWeekFragment.java
+++ app/src/main/java/com/example/checkingsystem/ChooseWeekFragment.java
@@ -8,7 +8,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
 
-
+//haha
 public class ChooseWeekFragment extends DialogFragment {
     int yourChoice;
     @Override
